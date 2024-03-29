e510bc82f042b99159b33c699ee6b7cddcf33e03
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-03-29 22:49:44 +0200

    feat(client): added not found page

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,5 +1,7 @@
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { ChevronLeft } from "lucide-react";
 import Sidebar from "@/components/Sidebar";
+import Button from "@/ui/Button";
 
 export const Route = createRootRoute({
     component: () => (
@@ -21,5 +23,38 @@ export const Route = createRootRoute({
                 <Outlet />
             </main>
         </div>
+    ),
+    notFoundComponent: () => (
+        <section
+            className={`
+                px-4 py-10 text-center
+                sm:px-6
+                lg:flex lg:h-full lg:flex-col lg:items-center lg:justify-center lg:px-8
+            `}
+        >
+            <h1
+                className={`
+                    block text-7xl font-bold text-gray-800
+                    dark:text-white
+                    sm:text-9xl
+                `}
+            >
+                404
+            </h1>
+            <p
+                className={`
+                    text-gray-600
+                    dark:text-gray-400
+                `}
+            >
+                Looks like the page you&apos;re looking for doesn&apos;t exist.
+            </p>
+            <Button asChild variant="link" className="mt-5">
+                <Link to="/">
+                    <ChevronLeft size={16} />
+                    Back to home
+                </Link>
+            </Button>
+        </section>
     ),
 });
