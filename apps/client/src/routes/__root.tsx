4499752fc0df38b0fc8a9ff626a14d704216939c
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-03-30 20:04:08 +0200

    refactor(client): moved pages components to separate constants

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -3,58 +3,62 @@ import { ChevronLeft } from "lucide-react";
 import Sidebar from "@/components/Sidebar";
 import Button from "@/ui/Button";
 
+const Page = () => (
+    <div
+        className={`
+            flex h-screen flex-col
+            lg:flex-row
+        `}
+    >
+        <Sidebar />
+        <main
+            className={`
+                w-full grow bg-background p-4 text-foreground
+                sm:p-6
+                md:px-8 md:py-7
+                lg:pl-12 lg:pr-14
+            `}
+        >
+            <Outlet />
+        </main>
+    </div>
+);
+
+const NotFound = () => (
+    <section
+        className={`
+            px-4 py-10 text-center
+            sm:px-6
+            lg:flex lg:h-full lg:flex-col lg:items-center lg:justify-center lg:px-8
+        `}
+    >
+        <h1
+            className={`
+                block text-7xl font-bold text-gray-800
+                dark:text-white
+                sm:text-9xl
+            `}
+        >
+            404
+        </h1>
+        <p
+            className={`
+                text-gray-600
+                dark:text-gray-400
+            `}
+        >
+            Looks like the page you&apos;re looking for doesn&apos;t exist.
+        </p>
+        <Button asChild variant="link" className="mt-5">
+            <Link to="/">
+                <ChevronLeft size={16} />
+                Back to home
+            </Link>
+        </Button>
+    </section>
+);
+
 export const Route = createRootRoute({
-    component: () => (
-        <div
-            className={`
-                flex h-screen flex-col
-                lg:flex-row
-            `}
-        >
-            <Sidebar />
-            <main
-                className={`
-                    w-full grow bg-background p-4 text-foreground
-                    sm:p-6
-                    md:px-8 md:py-7
-                    lg:pl-12 lg:pr-14
-                `}
-            >
-                <Outlet />
-            </main>
-        </div>
-    ),
-    notFoundComponent: () => (
-        <section
-            className={`
-                px-4 py-10 text-center
-                sm:px-6
-                lg:flex lg:h-full lg:flex-col lg:items-center lg:justify-center lg:px-8
-            `}
-        >
-            <h1
-                className={`
-                    block text-7xl font-bold text-gray-800
-                    dark:text-white
-                    sm:text-9xl
-                `}
-            >
-                404
-            </h1>
-            <p
-                className={`
-                    text-gray-600
-                    dark:text-gray-400
-                `}
-            >
-                Looks like the page you&apos;re looking for doesn&apos;t exist.
-            </p>
-            <Button asChild variant="link" className="mt-5">
-                <Link to="/">
-                    <ChevronLeft size={16} />
-                    Back to home
-                </Link>
-            </Button>
-        </section>
-    ),
+    component: Page,
+    notFoundComponent: NotFound,
 });
--- apps/client/src/routes/index.lazy.tsx
+++ apps/client/src/routes/index.lazy.tsx
@@ -1,5 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 
+const Page = () => <div>Hello /!</div>;
+
 export const Route = createLazyFileRoute("/")({
-    component: () => <div>Hello /!</div>,
+    component: Page,
 });
