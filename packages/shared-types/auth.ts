a8e0fe067a55c316d3ee4e5b7c11448eb3e82519
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-03-31 15:55:24 +0300

    feat(deps): created shared types package

---
+++ packages/shared-types/auth.ts
@@ -0,0 +1,67 @@
+import { z as zod } from "zod";
+
+const email = zod.string().min(1, { message: "Enter your email" });
+const emailChecked = email.email({ message: "Enter valid email" });
+const password = zod.string().trim().min(1, { message: "Enter your password" });
+const passwordChecked = password
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(255, { message: "Password must be at most 255 characters long" });
+
+export const emailSchema = zod.object({
+    email: emailChecked,
+});
+
+export const passwordSchema = zod.object({
+    password,
+});
+
+export const signUpSchema = zod.object({
+    email: emailChecked,
+    password: passwordChecked,
+});
+
+export const signUpSchemaWithPasswordConfirmation = zod
+    .object({
+        email: emailChecked,
+        password: passwordChecked,
+        confirmPassword: zod.string().trim().min(1, { message: "Confirm your password" }),
+    })
+    .refine((schemaData) => schemaData.password === schemaData.confirmPassword, {
+        message: "Passwords must match",
+        path: ["confirmPassword"],
+    });
+
+export const signInSchema = zod.object({
+    email,
+    password,
+});
+
+export const userResponseSchema = zod.object({
+    user: zod.object({
+        id: zod.string(),
+        email: zod.string(),
+        verifiedAt: zod.date().nullable(),
+        updatedAt: zod.date(),
+        createdAt: zod.date(),
+    }),
+});
+
+export const verificationCodeSchema = zod.object({
+    code: zod
+        .string()
+        .trim()
+        .min(5, { message: "Enter verification code" })
+        .max(5, { message: "Enter verification code" }),
+});
+
+export const resetPasswordSchema = zod.object({
+    token: zod.string().trim(),
+});
+
+export type EmailInput = zod.infer<typeof emailSchema>;
+export type PasswordInput = zod.infer<typeof passwordSchema>;
+export type SignUpInput = zod.infer<typeof signUpSchema>;
+export type SignUpInputWithPasswordConfirmation = zod.infer<typeof signUpSchemaWithPasswordConfirmation>;
+export type SignInInput = zod.infer<typeof signInSchema>;
+export type VerificationCodeInput = zod.infer<typeof verificationCodeSchema>;
+export type ResetPasswordInput = zod.infer<typeof resetPasswordSchema>;
