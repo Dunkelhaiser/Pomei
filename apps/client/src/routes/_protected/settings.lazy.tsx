196479cdb7be70ece748df379ba725d072df18c5
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-04 13:44:31 +0300

    refactor(client): used section component

--- apps/client/src/routes/_protected/settings.lazy.tsx
+++ apps/client/src/routes/_protected/settings.lazy.tsx
@@ -1,6 +1,7 @@
 import { Link, Outlet, createLazyFileRoute } from "@tanstack/react-router";
 import { useSignOut } from "@/api/auth/hooks";
 import Button from "@/ui/Button";
+import { Section, SectionContent, SectionHeader } from "@/ui/Section";
 import Separator from "@/ui/Separator";
 import { cn } from "@/utils/utils";
 
@@ -20,18 +21,11 @@ const Page = () => {
     const signOutHandler = useSignOut();
 
     return (
-        <section
-            className={`
-                flex flex-1 flex-col gap-4
-                md:gap-8
-            `}
-        >
-            <div className="mx-auto grid w-full max-w-6xl gap-2">
-                <h1 className="text-3xl font-semibold">Settings</h1>
-            </div>
-            <div
+        <Section>
+            <SectionHeader>Settings</SectionHeader>
+            <SectionContent
                 className={`
-                    mx-auto grid w-full max-w-6xl items-start gap-6
+                    grid items-start gap-6
                     md:grid-cols-[180px_1fr]
                     lg:grid-cols-[250px_1fr]
                 `}
@@ -65,8 +59,8 @@ const Page = () => {
                 <div className="grid gap-6">
                     <Outlet />
                 </div>
-            </div>
-        </section>
+            </SectionContent>
+        </Section>
     );
 };
 
