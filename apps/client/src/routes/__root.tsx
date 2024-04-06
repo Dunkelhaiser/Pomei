725fa3e489092b9965e1e841ae2a6de470def36b
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-06 19:05:56 +0300

    fix(client): fixed scrollbar in place

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -20,7 +20,7 @@ const Page = () => {
             <Sidebar />
             <main
                 className={`
-                    relative w-full grow bg-background p-4 text-foreground
+                    relative h-screen w-full grow overflow-auto bg-background p-4 text-foreground
                     sm:p-6
                     md:px-8 md:py-7
                     lg:pl-12 lg:pr-14
