5b3719901827501aee4588a919ac3a331d292b53
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-20 16:55:55 +0300

    fix(client): temporary removed PWA

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,43 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tsconfigPaths(),
-        TanStackRouterVite(),
-        VitePWA({
-            registerType: "autoUpdate",
-            manifest: {
-                name: "Pomei",
-                short_name: "Pomei",
-                start_url: "/",
-                display: "standalone",
-                background_color: "#f9fafb",
-                lang: "en",
-                scope: "/",
-                icons: [
-                    {
-                        src: "/android-chrome-192x192.png",
-                        sizes: "192x192",
-                        type: "image/png",
-                        purpose: "any maskable",
-                    },
-                    {
-                        src: "/android-chrome-512x512.png",
-                        sizes: "512x512",
-                        type: "image/png",
-                        purpose: "any maskable",
-                    },
-                ],
-                theme_color: "#f9fafb",
-            },
-        }),
-    ],
+    plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
     test: {
         globals: true,
         environment: "happy-dom",
