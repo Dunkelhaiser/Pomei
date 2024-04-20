413701d516349358e67b92506fc9ffa3f595c5bd
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-20 19:57:26 +0300

    fix(server): changed port

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -61,7 +61,7 @@ void (async () => {
     void app.register(foldersRoutes, { prefix: "/folders" });
 
     try {
-        await app.listen({ port: 3000, host: "0.0.0.0" });
+        await app.listen({ port: 10000, host: "0.0.0.0" });
     } catch (err) {
         app.log.error(err);
         process.exit(1);
