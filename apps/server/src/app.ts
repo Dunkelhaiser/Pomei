cf278b9f931f8236a51e3e40653fe12e014e07a8
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-20 22:09:02 +0300

    fix(server): reverted to strict

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -45,9 +45,9 @@ void (async () => {
         secret: "EGt+lNs9nf6rT8WGSQuGMYK4KCs=",
         setOptions: {
             expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-            secure: true,
+            secure: env.PRODUCTION === true,
             httpOnly: true,
-            sameSite: "none",
+            sameSite: "strict",
         },
     } as FastifyCookieOptions);
 
--- apps/server/src/modules/auth/auth.ts
+++ apps/server/src/modules/auth/auth.ts
@@ -3,13 +3,14 @@ import { Lucia } from "lucia";
 import { type Session, type User } from "lucia";
 import { db } from "@/db/client.ts";
 import { sessions, users } from "@/db/schema.ts";
+import { env } from "@/env.ts";
 
 const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
 
 export const lucia = new Lucia(adapter, {
     sessionCookie: {
         attributes: {
-            secure: true,
+            secure: env.PRODUCTION === true,
         },
     },
     getUserAttributes: (attributes) => ({
