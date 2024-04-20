db00467efc55d7ca98234133f95dcf1ccbbf402e
Dunkelhaiser <103267805+Dunkelhaiser@users.noreply.github.com>
2024-04-20 23:46:23 +0300

    fix(server): fixed number env

--- apps/server/src/env.ts
+++ apps/server/src/env.ts
@@ -10,7 +10,7 @@ export const env = createEnv({
             .transform((s) => s === "true"),
         RESEND_KEY: z.string(),
         FRONTEND_URL: z.string().url(),
-        PORT: z.number(),
+        PORT: z.coerce.number(),
     },
     runtimeEnv: process.env,
     emptyStringAsUndefined: true,
